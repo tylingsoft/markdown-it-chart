a6a08889f54339f492d350369c8804a8e15713cb
Tyler Long <tyler4long@gmail.com>
2017-04-01 20:21:13 +0800

    Simplify code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-const chartBlock = (code) => {
-  try {
-    let json = JSON.parse(code)
-    return `<canvas class="chartjs">${JSON.stringify(json)}</canvas>`
-  } catch (e) { // JSON.parse exception
-    return `<pre>${e}</pre>`
-  }
-}
-
 const ChartPlugin = (md) => {
   const temp = md.renderer.rules.fence.bind(md.renderer.rules)
   md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
-    let token = tokens[idx]
-    let code = token.content.trim()
+    const token = tokens[idx]
     if (token.info === 'chart') {
-      return chartBlock(code)
+      const code = token.content.trim()
+      try {
+        const json = JSON.parse(code)
+        return `<canvas class="chartjs">${JSON.stringify(json)}</canvas>`
+      } catch (e) { // JSON.parse exception
+        return `<pre>${e}</pre>`
+      }
     }
     return temp(tokens, idx, options, env, slf)
   }
